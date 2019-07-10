b7b03cef11f876f83e679800d59c477afaa64f5e
Kelsey Low <kklowlow@gmail.com>
2019-07-10 02:06:53 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,8 @@ class SessionsController < ApplicationController
 
   def create
     if params[:name].nil? || params[:name].empty?
+      redirect_to sessions_new_path
+    else
       session[:name] = params[:name]
       redirect_to '/'
   end
