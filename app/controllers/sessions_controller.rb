f7bcb0e617492c0c4bd6f577e435bd25bdf9bc17
Kelsey Low <kklowlow@gmail.com>
2019-07-10 02:05:57 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,11 @@
 class SessionsController < ApplicationController
+  def new 
+  end 
 
+  def create 
+    session[:username] = params[:username]
+    redirect_to '/'
+  end
 
   def destroy
     session.delete :username
